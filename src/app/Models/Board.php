2286f6de8fdf415053090e564b52e41c84972357
ahmedmohamed24 <ahmedmohamed24.dev@gmail.com>
2021-07-26 10:09:51 +0200

    [add-feature] ide-helper package install

--- src/app/Models/Board.php
+++ src/app/Models/Board.php
@@ -5,6 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Board
+ *
+ * @method static \Database\Factories\BoardFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Board newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Board newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Board query()
+ * @mixin \Eloquent
+ */
 class Board extends Model
 {
     use HasFactory;
